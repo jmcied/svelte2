b4f19a4c9619b0e19ebe5969b0e2f3d3bae7a09a
John McDonald <96137405+jmcied@users.noreply.github.com>
2023-05-28 19:09:24 +0100

    added hover over link text

--- src/lib/MainNavigator.svelte
+++ src/lib/MainNavigator.svelte
@@ -1,17 +1,17 @@
 <div class="box has-text-centered columns m-2">
-	<a href="/addPlace" class="column">
+	<a href="/addPlace" class="column" title="add place">
 		<i class="fas fa-map-pin fa-3x" style="color:rgb(153, 196, 74)" />
 	</a>
-	<a href="/report" class="column ">
+	<a href="/report" class="column" title="report">
 		<i class="fas fa-th-list fa-3x" style="color:rgb(63, 122, 139)" />
 	</a>
-	<a href="/map" class="column ">
+	<a href="/map" class="column" title="map">
 		<i class="fas fa-map-marked-alt fa-3x" style="color:rgb(102, 153, 255)" />
 	</a>
-	<a href="/charts" class="column is-2 mx-2">
+	<a href="/charts" class="column is-2 mx-2" title="charts">
         <i class="fas fa-chart-line fa-3x" style="color:rgb(149, 93, 176)" />
     </a>
-	<a href="/logout" class="column">
+	<a href="/logout" class="column" title="logout">
 		<i class="fas fa-sign-out-alt fa-3x" style="color:rgb(156, 70, 128)" />
 	</a>
 </div>
--- src/lib/WelcomeNavigator.svelte
+++ src/lib/WelcomeNavigator.svelte
@@ -1,8 +1,8 @@
 <div class="box has-text-centered columns m-2">
-  <a href="/signup" class="column">
+  <a href="/signup" class="column" title="signup">
     <i class="fas fa-user-cog fa-3x" style="color:rgb(63, 160, 155)"></i>
   </a>
-  <a href="/login" class="column">
+  <a href="/login" class="column" title="login">
     <i class="fas fa-user-edit fa-3x" style="color:green" title="Log in"></i>
   </a>
 </div>
